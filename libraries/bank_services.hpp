4ac712ae8320d01df2a1d9c03485febcfc1bd094
Omkar Sonawane <163371960+OmkarSonawane1230@users.noreply.github.com>
2025-07-24 12:29:25 +0000

    new file:   .vscode/tasks.json 	new file:   libraries/bank_services.hpp 	new file:   main 	new file:   main.cpp

---
+++ libraries/bank_services.hpp
@@ -0,0 +1,22 @@
+#include <cstring>
+#include <bits/stdc++.h>
+#include <iostream>
+#ifndef BANK_SERVICES_H
+#define BANK_SERVICES_H
+
+using namespace std;
+
+// Function declaration
+int add(int a, int b) {
+    return (a + b);
+}
+
+// Class declaration
+class MyClass {
+public:
+    void doSomething() {
+        cout << setw(20) << "My name is omkar";
+    }
+};
+
+#endif // BANK_SERVICES_H
